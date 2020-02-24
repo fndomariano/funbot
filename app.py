2141cd96f8f2802e4d603058f03f7d4015576d34
Fernando <fernando.mar16@gmail.com>
2020-02-24 19:50:57 -0300

    ajuste

--- app.py
+++ app.py
@@ -2,14 +2,7 @@
 
 from app_listener import AppListener
 
-def main():
-	listener = AppListener()
-
-	print('Searching new messages...')
-
-	while True:
-		listener.register_messages()
 		
-
 if __name__ == '__main__':
-	main()
+	listener = AppListener()
+	listener.register_messages()
